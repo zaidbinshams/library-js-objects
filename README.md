cc769517a1f491e4be67ca1277cef146cb7959c1
Zaid <142177842+zaidbinshams@users.noreply.github.com>
2025-09-05 19:10:03 +0530

    books can now be displayed

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# library-js-objects
+# 05-09-2025
+- back button on every screen
+- make the scrollbar track transparent
+- add shadow to the bottom of book-list if there is overflow
+- add read/unread button to bookCard elements
+- add remove button to bookCard elements
--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const display = document.querySelector("button.display");
 const addNew = document.querySelector("button.add-new");
 const readStat = document.querySelector("button.read");
 const remove = document.querySelector("button.remove");
+const main = document.querySelector(".main");
+const landing = document.querySelector(".landing");
+const bookList = document.querySelector(".book-list");
+const newForm = document.querySelector(".new-form");
 
 function Book(id, title, author, readStat) {
     this.id = id;
@@ -19,14 +23,62 @@ function addBook(title, author) {
     library.push(newBook);
 }
 
+addBook("test book 1", "test author 1");
+addBook("test book 2", "test author 2");
+addBook("test book 1", "test author 1");
+addBook("test book 2", "test author 2");
+addBook("test book 1", "test author 1");
+addBook("test book 2", "test author 2");
+addBook("test book 1", "test author 1");
+addBook("test book 2", "test author 2");
+addBook("test book 1", "test author 1");
+addBook("test book 2", "test author 2");
+addBook("test book 1", "test author 1");
+addBook("test book 2", "test author 2");
+addBook("test book 1", "test author 1");
+addBook("test book 2", "test author 2");
+
+
 function displayBooks() {
-    for (let i = 0; i < library.length; i++) {
-        console.log(library[i]);
+    landing.style.display = "none";
+    newForm.style.display = "none";
+    bookList.style.display = "flex";
+    bookList.replaceChildren();
+    if (library.length === 0) {
+        const bookCard = document.createElement("div");
+        bookCard.textContent = "Your library is currently empty.";
+        bookCard.style.color = "white";
+        bookCard.style.justifyContent = "center";
+        bookCard.style.alignItems = "center";
+        bookCard.style.fontFamily = "Rubik, sans-serif";
+        bookCard.style.fontWeight = "bold";
+        bookCard.style.fontSize = "large";
+        bookList.appendChild(bookCard);
+    } else {
+        for (let i = 0; i < library.length; i++) {
+            const bookCard = document.createElement("div");
+            bookCard.innerHTML = `<strong>Book ID: ${library[i].id}</strong><br>${library[i].title} by ${library[i].author}<br>(read: ${library[i].readStat})`;
+            bookCard.style.color = "rgb(40, 50, 20)";
+            bookCard.style.justifyContent = "center";
+            bookCard.style.alignItems = "center";
+            bookCard.style.fontFamily = "Rubik, sans-serif";
+            bookCard.style.border = "2px solid black";
+            bookCard.style.borderRadius = "8px";
+            bookCard.style.padding = "0.5rem 0.8rem";
+            bookCard.style.minHeight = "fit-content";
+            bookCard.style.backgroundColor = "rgb(150, 190, 60)";
+            bookList.appendChild(bookCard);
+        }
     }
 }
 
 function inputForm() {
-    // figure out how to display the form and send the data back to the library array
+    landing.style.display = "none";
+    bookList.style.display = "none";
+    newForm.style.display = "flex";
+    // create the form
+    const form = document.createElement("");
+    // store the value of all input fields into variables and then pass them as arguments to the addBook() function
 }
 
 function readStatus() {
